79d2423edfead7d11d8a51fd43a61b8a961a449a
Papaya Labs <papayalabs@gmail.com>
2019-11-01 17:59:25 +0100

    Second Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  resources :jokes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  get '/random', to: 'jokes#random', as: :random_joke
-  post '/search', to: 'jokes#search', as: :search_joke
-  post '/category', to: 'jokes#category', as: :category_joke
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/, defaults: {locale: "en"} do
+    resources :jokes
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'home#index'
+    get '/random', to: 'jokes#random', as: :random_joke
+    post '/search', to: 'jokes#search', as: :search_joke
+    post '/category', to: 'jokes#category', as: :category_joke
+  end
 end
